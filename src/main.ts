0e609a9884c80a1319cd07d73c5c739ac6cfd58b
Eric Hähner <eric.haehner@telekom.de>
2026-01-24 14:15:46 +0100

    clean up deprecated packages

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [
